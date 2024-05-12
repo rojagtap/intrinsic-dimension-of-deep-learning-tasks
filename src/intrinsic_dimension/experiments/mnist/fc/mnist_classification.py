c8b14cb7cdb128c673e48ef1957751be43fa73ab
Rohan Jagtap <rohanj30@icloud.com>
2024-05-12 23:12:00 +0530

    Refactor training (train, eval) utils to util module for reducing duplication

--- src/intrinsic_dimension/experiments/mnist/fc/mnist_classification.py
+++ src/intrinsic_dimension/experiments/mnist/fc/mnist_classification.py
@@ -6,48 +6,13 @@ from torch.utils.data import DataLoader
 
 from ....util.constants import DEVICE
 from ....util.data import get_dataset
-from ....util.util import set_seed, count_params
+from ....util.util import set_seed, count_params, train, evaluate_accuracy
 from ....util.plotter import plot_results, plot_model
 from ....wrappers.modeling_fc import SequentialSubspaceWrapper, FC
 
 
-def train(model, optimizer, batch_size, dataset, epochs):
-    """
-    train the model for the provided dataset
-    """
-    model.train()
-    for epoch in range(epochs):
-        total_loss = 0
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-        for step, (images, labels) in enumerate(dataloader):
-            optimizer.zero_grad()
-
-            images, labels = images.to(DEVICE), labels.to(DEVICE)
-            logits = model(images)
-            loss = torch.nn.functional.cross_entropy(logits, labels)
-
-            loss.backward()
-            optimizer.step()
-
-            total_loss += loss.item()
-
-        print(f'Epoch {epoch + 1}/{epochs}, Loss: {total_loss / (len(dataset) // batch_size)}')
-
-
-def evaluate(model, dataset, batch_size):
-    """
-    get test accuracy
-    """
-    model.eval()
-    dataloader = DataLoader(dataset, batch_size=batch_size)
-
-    correct = 0
-    for step, (images, labels) in enumerate(dataloader):
-        images, labels = images.to(DEVICE), labels.to(DEVICE)
-        logits = model(images)
-        correct += torch.sum(logits.argmax(1) == labels).item()
-
-    return 100 * correct / len(dataset)
+def accuracy_criterion(logits, labels):
+    return torch.sum(logits.argmax(1) == labels).item()
 
 
 if __name__ == '__main__':
@@ -79,8 +44,8 @@ if __name__ == '__main__':
     print("n_params in base model: ", n_params)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    train(model, optimizer, batch_size, train_dataset, epochs)
-    baseline_accuracy = evaluate(model, train_dataset, batch_size)
+    train(model, torch.nn.functional.cross_entropy, optimizer, batch_size, train_dataset, epochs)
+    baseline_accuracy = evaluate_accuracy(model, accuracy_criterion, train_dataset, batch_size)
     print(f"Baseline Accuracy: {baseline_accuracy}")
     plot_model(model, "baseline", basedir, sample_images, torch.nn.functional.cross_entropy, sample_labels)
 
@@ -101,8 +66,8 @@ if __name__ == '__main__':
         base_model = FC(input_size=sample_images.size()[1:], hidden_size=hidden_size, num_classes=num_classes).to(DEVICE)
         model = SequentialSubspaceWrapper(base_model=base_model, dint=dint).to(DEVICE)
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-        train(model, optimizer, batch_size, train_dataset, epochs)
-        history[dint] = evaluate(model, test_dataset, batch_size)
+        train(model, torch.nn.functional.cross_entropy, optimizer, batch_size, train_dataset, epochs)
+        history[dint] = evaluate_accuracy(model, accuracy_criterion, test_dataset, batch_size)
         print(f"Accuracy: {history[dint]}, for dint: {dint}")
 
         if dint - 1 == 1000:
--- src/intrinsic_dimension/util/util.py
+++ src/intrinsic_dimension/util/util.py
@@ -3,6 +3,9 @@ import random
 
 import numpy as np
 import torch
+from torch.utils.data import DataLoader
+
+from .constants import DEVICE
 
 
 def set_seed(seed):
@@ -27,3 +30,42 @@ def count_params(model):
             total_params += param.numel()
 
     return total_params
+
+
+def train(model, loss_fn, optimizer, batch_size, dataset, epochs):
+    """
+    train the model for the provided dataset
+    """
+    model.train()
+    for epoch in range(epochs):
+        total_loss = 0
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        for step, (batch, labels) in enumerate(dataloader):
+            optimizer.zero_grad()
+
+            batch, labels = batch.to(DEVICE), labels.to(DEVICE)
+            logits = model(batch)
+            loss = loss_fn(logits, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        print(f'Epoch {epoch + 1}/{epochs}, Loss: {total_loss / (len(dataset) // batch_size)}')
+
+
+def evaluate_accuracy(model, criterion, dataset, batch_size):
+    """
+    get test accuracy
+    """
+    model.eval()
+    dataloader = DataLoader(dataset, batch_size=batch_size)
+
+    correct = 0
+    for step, (batch, labels) in enumerate(dataloader):
+        batch, labels = batch.to(DEVICE), labels.to(DEVICE)
+        logits = model(batch)
+        correct += criterion(logits, labels)
+
+    return 100 * correct / len(dataset)
